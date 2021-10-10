a9f43d7e5779de706bfa98a3c73a0c38a56edf17
Guilherme Quintino <gui23091991@gmail.com>
2021-10-10 16:43:23 -0300

    Insertion from Swall alert.

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 import { SendCash } from '../placeholder/send-cash';
 import { ServiceService } from './../services/service.service';
-
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -31,7 +31,28 @@ export class TableComponent implements OnInit {
   // }
   trash(id: number) {
     this.service.remove(id).subscribe(()=> {
-    window.location.reload();
+      Swal.fire('Deletado com sucesso!').then(() => {
+        
+      })
+      Swal.fire({
+        title: 'Está certo disso?',
+        text: "Voce não poderá reverter a exclusão!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: 'gray',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, deletar!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Deletado!',
+            'Seus arquivos foram deletados com sucesso!.',
+            'success'
+          ).then(()=> {
+            window.location.reload();
+          })
+        }
+      })
     })
   }
 }
